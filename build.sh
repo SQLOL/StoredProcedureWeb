2d2172099909d747a06592d0842987353846fae5
Justin Martin <frozenfire@thefrozenfire.com>
2013-12-21 13:38:37 -0800

    Removed extraneous dump flags. Check for database parameter. Dump Assets as one command.

--- build.sh
+++ build.sh
@@ -5,9 +5,13 @@ BUILDDIR=`mktemp -d`
 DBNAME=${@: -1}
 WORKINGDIR=`pwd`
 
-mysqldump --compact --compress --extended-insert --hex-blob --skip-lock-tables --single-transaction --ignore-table=$DBNAME.Asset $DUMPARGS  > "$BUILDDIR/install.sql"
-mysqldump --compact --compress --skip-lock-tables --single-transaction --no-data $DUMPARGS Asset >> "$BUILDDIR/install.sql"
-mysqldump --compact --compress --skip-lock-tables --single-transaction --no-create-info $DUMPARGS Asset >> "$BUILDDIR/install.sql"
+if [ -z $DBNAME ]; then
+    echo "No database specified";
+    exit;
+fi
+
+mysqldump --compact --compress --extended-insert --hex-blob --single-transaction --ignore-table=$DBNAME.Asset $DUMPARGS  > "$BUILDDIR/install.sql"
+mysqldump --compact --compress --single-transaction $DUMPARGS Asset >> "$BUILDDIR/install.sql"
 
 cp -r $WORKINGDIR/public $BUILDDIR
 

8bc7150186138d19be67fa1d9ea96d609ccfd198
Justin Martin <frozenfire@thefrozenfire.com>
2013-12-21 19:40:56 -0800

    Removed unnecessary empty check.

--- public/index.php
+++ public/index.php
@@ -1,42 +1,39 @@
 <?php
 $pdo = new PDO('mysql:dbname='.$_SERVER['DB_NAME'].';host='.$_SERVER['DB_HOST'], $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD']);
 
-if(!empty($_SERVER)) {
-    $pdo->exec('CALL request$init');
+$pdo->exec('CALL request$init');
 
-    $headerSetter = 'INSERT INTO Request$Header (name, value) VALUES'.PHP_EOL;
-    $headerParams = array();
-    $paramSetter = 'INSERT INTO Request$Param (name, value) VALUES'.PHP_EOL;
-    $paramParams = array();
-    foreach($_SERVER as $key => $value) {
-        if(substr($key, 0, 5) === 'HTTP') {
-            if(count($headerParams)) {
-                $headerSetter .= ',';
-            }
-            $headerSetter .= '(?, ?)';
-            $headerParams[] = $key;
-            $headerParams[] = $value;
-        } else {
-            if(count($paramParams)) {
-                $paramSetter .= ',';
-            }
-            $paramSetter .= '(?, ?)';
-            $paramParams[] = $key;
-            $paramParams[] = $value;
+$headerSetter = 'INSERT INTO Request$Header (name, value) VALUES'.PHP_EOL;
+$headerParams = array();
+$paramSetter = 'INSERT INTO Request$Param (name, value) VALUES'.PHP_EOL;
+$paramParams = array();
+foreach($_SERVER as $key => $value) {
+    if(substr($key, 0, 5) === 'HTTP') {
+        if(count($headerParams)) {
+            $headerSetter .= ',';
         }
-    }
-    
-    if(!empty($headerParams)) {
-        $headerStatement = $pdo->prepare($headerSetter);
-        $headerStatement->execute($headerParams);
-    }
-    
-    if(!empty($paramParams)) {
-        $paramStatement = $pdo->prepare($paramSetter);
-        $paramStatement->execute($paramParams);
+        $headerSetter .= '(?, ?)';
+        $headerParams[] = $key;
+        $headerParams[] = $value;
+    } else {
+        if(count($paramParams)) {
+            $paramSetter .= ',';
+        }
+        $paramSetter .= '(?, ?)';
+        $paramParams[] = $key;
+        $paramParams[] = $value;
     }
 }
 
+if(!empty($headerParams)) {
+    $headerStatement = $pdo->prepare($headerSetter);
+    $headerStatement->execute($headerParams);
+}
+
+if(!empty($paramParams)) {
+    $paramStatement = $pdo->prepare($paramSetter);
+    $paramStatement->execute($paramParams);
+}
 
 $statement = $pdo->prepare('CALL request (
     :request_method,

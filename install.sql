04e131ae677b5d87dddbd163b14931f8e84f0868
Justin Martin <frozenfire@thefrozenfire.com>
2014-04-05 14:28:53 -0700

    Moved View into its own repository.

--- install.sql
+++ install.sql
@@ -3,6 +3,7 @@ source vendor/Config/Module.sql
 source vendor/HTTP/Module.sql
 source vendor/SQLUnit/Module.sql
 source vendor/StdLib/Module.sql
+source vendor/View/Module.sql
 source src/StoredProcedureWeb.sql
 
 source application/config.sql
--- src/StoredProcedureWeb.sql
+++ src/StoredProcedureWeb.sql
@@ -1,10 +1,8 @@
 delimiter ;
-source src/schema/View.sql
 source src/schema/Application.sql
 source src/schema/Module.sql
 
 delimiter |
-source src/procedures/View.sql
 source src/procedures/Application.sql
 source src/procedures/Module.sql
 delimiter ;

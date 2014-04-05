e7155a7d10ffb850195228eb0d1ecfa3db83e0c2
Justin Martin <frozenfire@thefrozenfire.com>
2014-04-05 14:43:23 -0700

    Moved MVC into its own repository. Moved config out of main repo, into component repos. Eliminated src directory entirely.

--- install.sql
+++ install.sql
@@ -1,13 +1,16 @@
-source vendor/AssetManager/Module.sql
 source vendor/Config/Module.sql
-source vendor/HTTP/Module.sql
 source vendor/ModuleManager/Module.sql
+source vendor/AssetManager/Module.sql
+source vendor/HTTP/Module.sql
+source vendor/MVC/Module.sql
 source vendor/SQLUnit/Module.sql
 source vendor/StdLib/Module.sql
 source vendor/View/Module.sql
-source src/StoredProcedureWeb.sql
 
 source application/config.sql
 source application/modules.sql
 source tests/sqlunit.sql
+
+CALL module$register_module ('application', TRUE);
+
 CALL module$load_modules;
--- src/config.sql
+++
@@ -1,4 +0,0 @@
-CALL config$set ('base_path', '/var/www');
-CALL config$set ('path_delimiter', '/');
-CALL config$set ('application$route$not-found', 'error/not-found');
-CALL config$set ('application$route$internal-server-error', 'error/internal-server-error');
